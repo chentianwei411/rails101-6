337236e878ed978d5bafb6ff1842e0b8699821cc
chentianwei <ctw.3@163.com>
2017-11-07 20:30:47 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
+  before_action :find_group_and_check_permission, only:[:edit,:update,:destroy]
 
   def index
     @groups = Group.all
@@ -24,20 +25,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert:"You have no permission"
-    end
-
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert:"You have no permission"
-    end
 
     if @group.update(group_params)
       flash[:notice] = "变更成功"
@@ -48,13 +38,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert:"You have no permission"
-
-    end
-
     @group.destroy
 
     flash[:warning] = "删除成功"
@@ -66,5 +49,13 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:title,:description)
+  end
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert:"You have no permission"
+    end
   end
 end
