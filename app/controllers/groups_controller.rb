db169d44f97af6a65700c9ad465e92bc6c5382fe
chentianwei <ctw.3@163.com>
2017-11-07 10:10:50 +0800

    implement groups#edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,20 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:notice] = "变更成功"
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def group_params
